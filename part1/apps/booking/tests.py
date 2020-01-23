dc976d0aa618b389f815f57c33cf31b455d8c9ec
Sullivan Matas <bugounet@users.noreply.github.com>
2020-01-24 00:52:57 +0100

    Add tests to check upcoming "filter_by_user" method

--- part1/apps/booking/tests.py
+++ part1/apps/booking/tests.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from random import randint
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
@@ -47,3 +48,58 @@ class BookingCascadeRemovalTestCase(TestCase):
 
         # Then
         self.assertFalse(Booking.objects.filter(pk=booking.pk).exists())
+
+
+class BookingManagerTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.room = cls.create_room_resource()
+        cls.user_1 = cls.create_user("1")
+        cls.user_2 = cls.create_user("2")
+        cls.user_admin = cls.create_user("admin", admin=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.room.delete()
+        cls.user_1.delete()
+        cls.user_2.delete()
+        cls.user_admin.delete()
+
+    @classmethod
+    def create_room_resource(cls):
+        return Resource.objects.create(label="Room A", type="Conference-room")
+
+    @classmethod
+    def create_user(cls, name, admin=False):
+        User = get_user_model()
+        return User.objects.create(username=name, is_staff=admin)
+
+    def create_booking(self, user):
+        random_start = tznow()+timedelta(minutes=randint(100, 1000))
+        end = random_start+timedelta(minutes=5)
+        return Booking.objects.create(
+            resource=self.room,
+            owner=user,
+            start_datetime=random_start,
+            end_datetime=end
+        )
+
+    def test_booking_for_user_returns_all_bookings_when_admin(self):
+        # assuming you have 1 booking on user 1 and 2 bookings on user 2
+        self.create_booking(self.user_1)
+        self.create_booking(self.user_2)
+        self.create_booking(self.user_2)
+
+        self.assertEqual(Booking.objects.for_user(self.user_admin).count(), 3)
+
+    def test_booking_for_user_only_features_my_bookings(self):
+        # assuming you have 1 booking on user 1 and 2 bookings on user 2
+        self.create_booking(self.user_1)
+        self.create_booking(self.user_2)
+        self.create_booking(self.user_2)
+
+        self.assertEqual(Booking.objects.for_user(self.user_1).count(), 1)
+        self.assertEqual(
+            Booking.objects.for_user(self.user_1)[0].owner_id,
+            self.user_1.id
+        )
