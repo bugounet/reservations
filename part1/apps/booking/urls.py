b79d9b350dadddc7217a698d7c8699b0d92666e8
Sullivan Matas <bugounet@users.noreply.github.com>
2020-01-25 18:14:07 +0100

    reduce import size

--- part1/apps/booking/urls.py
+++ part1/apps/booking/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
-from .views import BookingListView, BookingDetailsView, BookingCreationView
+from . import views
 
 urlpatterns = [
     path(
         'new-booking',
-        BookingCreationView.as_view(),
+        views.BookingCreationView.as_view(),
         name="create_booking_view"
     ),
     path(
         'booking/<int:pk>',
-        BookingDetailsView.as_view(),
+        views.BookingDetailsView.as_view(),
         name="booking_details"
     ),
-    path('bookings', BookingListView.as_view(), name="bookings_list"),
+    path(
+        'bookings',
+        views.BookingListView.as_view(),
+        name="bookings_list"
+    ),
 ]
