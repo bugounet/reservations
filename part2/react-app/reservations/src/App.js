a1bc85db35798a92951e41b5f1e44477530937b5
Sullivan Matas <bugounet@users.noreply.github.com>
2020-02-07 16:19:16 +0100

    Add a custom theme. Purple is ugly.

--- part2/react-app/reservations/src/App.js
+++ part2/react-app/reservations/src/App.js
@@ -14,6 +14,8 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 
 function App() {
     const [menuState, setMenuState] = useState(false);
@@ -21,32 +23,34 @@ function App() {
         setMenuState(oldState => !oldState);
     };
     return (
-        <Router>
-            <ApplicationBar onClick={toggleSideMenu}/>
-            <LeftSideMenu open={menuState} toggleDrawer={toggleSideMenu}/>
-            <Switch>
-                <Route path="/new-booking">
-                    <BookingPage />
-                </Route>
-                <Route path="/booking/:id">
-                    <BookingPage />
-                </Route>
-                <Route path="/bookings">
-                    <AllBookingsPage />
-                </Route>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <ApplicationBar onClick={toggleSideMenu}/>
+                <LeftSideMenu open={menuState} toggleDrawer={toggleSideMenu}/>
+                <Switch>
+                    <Route path="/new-booking">
+                        <BookingPage />
+                    </Route>
+                    <Route path="/booking/:id">
+                        <BookingPage />
+                    </Route>
+                    <Route path="/bookings">
+                        <AllBookingsPage />
+                    </Route>
 
-                <Route path="/resource/:id">
-                    <ResourcePage />
-                </Route>
-                <Route path="/resources">
-                    <AllResourcesPage />
-                </Route>
+                    <Route path="/resource/:id">
+                        <ResourcePage />
+                    </Route>
+                    <Route path="/resources">
+                        <AllResourcesPage />
+                    </Route>
 
-                <Route path="/">
-                    <MainPage />
-                </Route>
-            </Switch>
-        </Router>
+                    <Route path="/">
+                        <MainPage />
+                    </Route>
+                </Switch>
+            </Router>
+        </ThemeProvider>
     );
 }
 
