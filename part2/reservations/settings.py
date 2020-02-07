073c4df41b81eeb93f69b696cb5dffbda283b88d
Sullivan Matas <bugounet@users.noreply.github.com>
2020-02-07 16:20:11 +0100

    Use DRF tokens & bugfix django social login

--- part2/reservations/settings.py
+++ part2/reservations/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'social.apps.django_app.default',
+    'social_django',
     'apps.booking',
     'apps.profiles',
     'apps.resources',
@@ -156,6 +158,9 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
