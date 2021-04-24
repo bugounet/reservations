01ef30db7b70131a397d1beffefc976f3091a3a4
Bugounet <sullivan.matas@gmail.com>
2021-04-24 20:23:23 +0200

    Drop useless parts

--- part2/apps/website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from apps.website import views
-
-
-urlpatterns = [
-    path('', views.index_view, name="index"),
-    path('logout', views.logout_view, name="logout"),
-]
--- part3/reservations/router.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-
-from apps.booking.views import BookingViewSet
-from apps.resources.views import ResourceViewSet
-
-router = routers.DefaultRouter()
-router.register('booking', BookingViewSet)
-router.register('resource', ResourceViewSet)
